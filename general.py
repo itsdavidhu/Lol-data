f959c3d4b7d37533e097f2a1ac2e9b1de4d8305a
Hu <hu-dav@milacron.com>
2024-07-24 16:41:02 -0400

    match data

--- general.py
+++ general.py
@@ -4,17 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 riot_api = os.environ["RIOT_API"]
-api_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/PositivityArc/pck"
 
-api_url = api_url + "?api_key=" + riot_api
-print(api_url)
-response = requests.get(api_url)
+account_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/PositivityArc/pck"
+account_url = account_url + "?api_key=" + riot_api
+response = requests.get(account_url)
 player_info = response.json()
-
 puuid = player_info['puuid']
-matches_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/Q_iDlzDMhwd5XDZIMsbPnhrd68wp9r68WxFXESmJYZtCcsopxEFP2TH-Awn4gC7IoYf0fNW0eVktZg/ids?start=0&count=100"
+
+matches_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=100"
 matches_url = matches_url + "&api_key=" + riot_api
-print(matches_url)
 response = requests.get(matches_url)
 matches = response.json()
-print(matches)
+
+for match in matches:
+    match_api = "https://americas.api.riotgames.com/lol/match/v5/matches/" + match + "?api_key=" + riot_api
+    response = requests.get(match_api)
+    match_data = response.json()
