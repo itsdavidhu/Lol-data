fce9a42e4b38c22145314f056c33335fe1cfa654
Hu <hu-dav@milacron.com>
2024-07-29 09:45:54 -0400

    fixed api limit

--- general.py
+++ general.py
@@ -25,25 +25,22 @@ for rank in ranks:
             while num_accounts < 100:
                 curr_rank = "https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{0}/{1}?page={2}&api_key={3}".format(rank, division, str(page), riot_api)
                 response = requests.get(curr_rank)
-                if response.status_code == 429:
+                while response.status_code == 429:
                     time.sleep(10)
                     response = requests.get(curr_rank)
                 accounts = response.json()
-                try:
-                    for i in range(len(accounts)):
-                        curr_account = accounts[i]
-                        if curr_account["wins"] + curr_account["losses"] > 200:
-                            writer.writerow([curr_account["leagueId"], curr_account["queueType"], 
-                                curr_account["tier"], curr_account["rank"], curr_account["summonerId"], 
-                                curr_account["leaguePoints"], curr_account["wins"], curr_account["losses"], 
-                                curr_account["veteran"], curr_account["inactive"], curr_account["freshBlood"], 
-                                curr_account["hotStreak"]])
-                            num_accounts += 1
-                            if num_accounts >= 100:
-                                break
-                    page += 1
-                except:
-                    print(response.status_code)
+                for i in range(len(accounts)):
+                    curr_account = accounts[i]
+                    if curr_account["wins"] + curr_account["losses"] > 200:
+                        writer.writerow([curr_account["leagueId"], curr_account["queueType"], 
+                            curr_account["tier"], curr_account["rank"], curr_account["summonerId"], 
+                            curr_account["leaguePoints"], curr_account["wins"], curr_account["losses"], 
+                            curr_account["veteran"], curr_account["inactive"], curr_account["freshBlood"], 
+                            curr_account["hotStreak"]])
+                        num_accounts += 1
+                        if num_accounts >= 100:
+                            break
+                page += 1
 
 for rank in high_elo:
     file_path = "data/{0}".format(rank)
@@ -55,7 +52,7 @@ for rank in high_elo:
                          "freshBlood", "hotStreak"])
         curr_rank = "https://na1.api.riotgames.com/lol/league/v4/{0}/by-queue/RANKED_SOLO_5x5?api_key={1}".format(rank, riot_api)
         response = requests.get(curr_rank)
-        if response.status_code == 429:
+        while response.status_code == 429:
             time.sleep(10)
             response = requests.get(curr_rank)
         accounts = response.json()["entries"]
