daf9412da67f3fd8a76716c8a3b8b18bc88b7337
Hu <hu-dav@milacron.com>
2024-08-01 09:34:07 -0400

    more general stats

--- general_stats.py
+++ general_stats.py
@@ -11,13 +11,20 @@ for rank in ranks:
         file_path = "accounts/{0}.csv".format(rank)
         curr_rank = pd.read_csv(file_path)
         curr_rank['avg_win'] = curr_rank['wins'] / (curr_rank['losses'] + curr_rank['wins'])
-        winrates[rank] = curr_rank['avg_win'].median()
+        winrates[rank] = {}
+        info = curr_rank['avg_win'].describe()
+        for key in info.keys():
+            winrates[rank][key] = info[key]
     else:
         for division in divisions:
             file_path = "accounts/{0}_{1}.csv".format(rank, division)
             curr_rank = pd.read_csv(file_path)
             curr_rank['avg_win'] = curr_rank['wins'] / (curr_rank['losses'] + curr_rank['wins'])
-            winrates["{0}_{1}".format(rank, division)] = curr_rank['avg_win'].median()
+            rank_division = "{0}_{1}".format(rank, division)
+            winrates[rank_division] = {}
+            info = curr_rank['avg_win'].describe()
+            for key in info.keys():
+                winrates[rank_division][key] = info[key]
 winrates_json = json.dumps(winrates, indent=4)
 with open("winrates.json", "w") as file:
     file.write(winrates_json)
