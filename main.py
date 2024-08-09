f7433bc2596a9d41c2d60404ad368b23d98551d9
Hu <hu-dav@milacron.com>
2024-08-09 14:53:06 -0400

    update main.py for complete data collection

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from lol_data import LolData
 
 lol_data = LolData()
-lol_data.general_stats()
+lol_data.load_general_accounts()
+lol_data.verify_data()
+lol_data.load_high_elo_accounts()
+lol_data.load_general_matches()
+lol_data.load_high_elo_matches()
+lol_data.load_general_match_data()
+lol_data.load_high_elo_match_data()
