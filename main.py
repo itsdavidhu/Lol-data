f0c04af224682926ced4fbacfb4f83dd341fcbd7
Hu <hu-dav@milacron.com>
2024-08-09 15:00:08 -0400

    update to match conventions

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def collect_data():
     lol_data.load_general_match_data()
     lol_data.load_high_elo_match_data()
 
+def main():
+    collect_data()
+
 
 if __name__ == "__main__":
-    collect_data()
+    main()
+    
