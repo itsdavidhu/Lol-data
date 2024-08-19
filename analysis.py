7bf3e994774a429d7d95e9bd1798d36f221bb829
Hu <hu-dav@milacron.com>
2024-08-19 16:54:39 -0400

    possible bug

--- analysis.py
+++ analysis.py
@@ -2,13 +2,16 @@ import json
 import os
 
 class LolAnalysis():
-
     def __init__(self) -> None:
         self.ranks = ["DIAMOND", "EMERALD", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]
         self.divisions = ["I", "II", "III", "IV"]
         self.high_elo = ["masterleagues", "grandmasterleagues", "challengerleagues"]
 
     def opt_num_games(self):
+        k_wins = {}
+        for i in range(13):
+            k_wins[i] = [0, 0]
+        print(k_wins)
         path = "LolData"
         for rank in self.high_elo:
             rank_path = "{0}/{1}".format(path, rank)
@@ -18,7 +21,11 @@ class LolAnalysis():
                 k = 0
                 for game in os.listdir(account_path):
                     game_path = "{0}/{1}".format(account_path, game)
+                    print(game_path)
                     with open(game_path, "r") as file:
                         curr = json.load(file)
-                        print(curr['info']['gameStartTimestamp'], curr['info']['gameEndTimestamp'])
+                        start = curr['info']['gameStartTimestamp']
+                        end = curr['info']['gameEndTimestamp']
+                        # if start - last_game > 1800:
+
 
