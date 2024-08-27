c33251958dc89df73c366aa3c4cf8de302b39d5c
David Hu <hu.david.dh@gmail.com>
2024-08-26 21:10:54 -0400

    plot

--- plot.py
+++ plot.py
@@ -2,25 +2,55 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def winrate(games):
-    return games[0] / (games[0] + games[1])
-
 ranks = ["DIAMOND", "EMERALD", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]
 divisions = ["I", "II", "III", "IV"]
+high_elo = ["challengerleagues", "grandmasterleagues", "masterleagues"]
 rank_div = [[f"{rank}/{division}" for division in divisions] for rank in ranks]
 
-path = "session_winrates.json"
-sessions = pd.read_json(path)
-sessions = sessions.map(lambda x: winrate(x))
-for i in range(len(ranks)):
-    sessions[ranks[i]] = sessions[rank_div[i]].mean(axis=1)
-    sessions = sessions.drop(rank_div[i], axis=1)
+def session():
+    path = "session_winrates.json"
+    sessions = pd.read_json(path)
+    total = {}
+    for i in range(13):
+        total[i] = [0, 0]
+    for index, row in sessions.iterrows():
+        for rank in high_elo:
+            total[index][0] += row[rank][0]
+            total[index][1] += row[rank][1]
+        for rank in ranks:
+            for division in divisions:
+                total[index][0] += row[f"{rank}/{division}"][0]
+                total[index][1] += row[f"{rank}/{division}"][1]
 
-sessions = sessions.mean(axis=1)
+    for i in range(13):
+        total[i] = round(total[i][0] / (total[i][0] + total[i][1]), ndigits=3)
+    total['12+']= total.pop(12)
 
-sessions.plot.bar(ylim=(0.45, 0.55), xlabel="Number of games played in a session", ylabel="Winrate")
-ax = sessions.plot.bar()
+    sessions = pd.DataFrame.from_dict(total, orient='index')
+    sessions.index.name = "Number of games played"
+    ax = sessions.plot.bar(ylim=(0.45, 0.535), ylabel="Winrate")
 
-for container in ax.containers:
-    ax.bar_label(container)
-plt.show()
+    for container in ax.containers:
+        ax.bar_label(container)
+    ax.get_legend().remove()
+    plt.show()
+
+def breaks():
+    path = "breaks.json"
+    breaks = pd.read_json(path)
+    total = [0, 0]
+    for rank in high_elo:
+        total[0] += breaks[rank]['wins']
+        total[1] += breaks[rank]['losses']
+    for rank in ranks:
+        for division in divisions:
+            total[0] += breaks[f"{rank}/{division}"]['wins']
+            total[1] += breaks[f"{rank}/{division}"]['losses']
+    print(total[0] / (total[0] + total[1]))
+
+def streaks():
+    path = "streaks_winrates.json"
+    streaks = pd.read_json(path)
+    print(streaks)
+
+streaks()
